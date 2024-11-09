4cf67a6a0a09907a9d7dfe2b6f1f2f5605e14eaf
Sai Harshit B <sai.harshitbalaji@gmail.com>
2024-11-10 04:22:55 +0530

    Created a JSON to store global variables. They will store initial default values and some other default values.

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = global.config = {
+    details: {
+        Network: {
+            IP: "",
+        }
+        // rest of your translation object
+    }
+    // other global config variables you wish
+};
